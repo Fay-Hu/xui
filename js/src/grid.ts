5864d1e35eef3866fceef6440f0d1a53c13e34d7
Fay <479389165@qq.com>
2016-12-04 20:54:46 +0800

    test of first commit2

--- js/src/grid.ts
+++ js/src/grid.ts
@@ -9,17 +9,23 @@ const Grid = (($) => {
     const NS_svg = "http://www.w3.org/2000/svg";
 
     const CLASSES = {
-        stage: 'stage',
-        item: 'item',
-        dot: 'dot'
-    };
+            stage: 'stage',
+            item: 'item',
+            dot: 'dot'
+        },
+        ATTRS = {
+            position: 'data-pos'
+        },
+        SELECTORS = {
+            position:'[data-pos="${pos}"]'
+        };
     class Grid {
 
         public static defaults = {
-            dotSize:4,
+            dotSize: 4,
             dotPath: 'M 4,0 V 8 M 0,4 H 8 Z',
-            itemWid: 60,
-            itemHigh: 60
+            itemWid: 80,
+            itemHigh: 80
         };
 
         options: {};
@@ -31,7 +37,9 @@ const Grid = (($) => {
         constructor(element: $, options: {}) {
             this.element = element;
             this.options = $.extend({}, Grid.defaults, options);
-            this.$stage = $(document.createElementNS(NS_svg, 'svg')).attr('class', CLASSES.stage);
+            this.$stage = $(document.createElementNS(NS_svg, 'svg')).attr({
+                'class': CLASSES.stage
+            });
             this._init();
         }
 
@@ -44,23 +52,31 @@ const Grid = (($) => {
 
             for (let i = 0; i < this.xLength; i++) {
                 for (let j = 0; j < this.yLength; j++) {
-                    this._createItem(opts.itemWid * i, opts.itemHigh * j,).appendTo(this.$stage);
+                    this._createItem(opts.itemWid * i, opts.itemHigh * j,).attr(ATTRS.position, i + ',' + j).appendTo(this.$stage);
                 }
             }
             this.element.append(this.$stage);
+
+            var self = this;
+            this.$stage.on('mousemove',function (e) {
+                var offset = $(this).offset();
+                console.log(self._getClosestItem(e.pageX - offset.left,e.pageY - offset.top));
+            })
         }
 
         private _addRow() {
             var opts = this.options;
-            for (let i = 0; i < this.xLength + 1;) {
-                this._createItem(opts.itemWid * i, opts.itemHigh * (this.yLength)).appendTo(this.$stage);
+            this.xLength++;
+            for (let i = 0; i < this.yLength;) {
+                this._createItem(opts.itemWid * this.xLength, opts.itemHigh * i++).attr(ATTRS.position, this.xLength + ',' + i).appendTo(this.$stage);
             }
         }
 
-        private _addRow() {
+        private _addCol() {
             var opts = this.options;
-            for (let i = 0; i < this.YLength + 1; i++) {
-                this._createItem(opts.itemWid * (this.xLength + 1), opts.itemHigh * i).appendTo(this.$stage);
+            this.yLength++;
+            for (let i = 0; i < this.xLength;) {
+                this._createItem(opts.itemWid * i++, opts.itemHigh * this.yLength).attr(ATTRS.position, i + ',' + this.yLength).appendTo(this.$stage);
             }
         }
 
@@ -71,10 +87,18 @@ const Grid = (($) => {
                 transform: `translate(${x},${y})`,
                 class: CLASSES.item
             })
-            .append($(document.createElementNS(NS_svg, 'path')).attr({
-                d: opts.dotPath, class: CLASSES.dot,
-                transform: `translate(${opts.itemWid / 2 - opts.dotSize},${opts.itemHigh / 2 - opts.dotSize})`
-            }));
+                .append($(document.createElementNS(NS_svg, 'path')).attr({
+                    d: opts.dotPath, class: CLASSES.dot,
+                    transform: `translate(${opts.itemWid / 2 - opts.dotSize},${opts.itemHigh / 2 - opts.dotSize})`
+                }));
+        }
+
+        private _getClosestItem(cx, cy) {
+            var opts = this.options,
+                posX = Math.floor(cx/opts.itemWid),
+                posY = Math.floor(cy/opts.itemHigh);
+
+            return this.$stage.find(SELECTORS.position.replace(/\$\{pos\}/,posX + ',' + posY));
         }
 
         //释放对象
